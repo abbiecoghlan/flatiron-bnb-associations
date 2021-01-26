706fc61f77d6b527308d2dd9ae28937e58ad3e93
“Abbie <abbie.coghlan@gmail.com>
2021-01-26 08:52:07 -0600

    done

---
+++ app/helpers/r_neighborhoods_helper.rb
@@ -0,0 +1,2 @@
+module RNeighborhoodsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :neighborhoods
+  resources :reservations
+  resources :reviews
   resources :users
 
   resources :listings
